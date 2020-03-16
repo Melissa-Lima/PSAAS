bc2c53ec7f373823b152f6429a9de3185a39466f
Melissa Lima <melissa.lima@openstartups.net>
2020-03-16 19:32:48 -0300

    first commit

---
+++ projectPhase.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Project {
+    public:
+    // aqui eu quero puxar um vetor de requisitos do arquivo recquirementsPhase.cpp. Não sei exatamente como fazer essa iteração...
+        void requisitesInteration() {
+            for(int i=0; i < requisitos.length(); i++) {
+
+            }
+        }
+
+    // depois criar uma função que printa as subfases da fase de projeto.
+};
---
+++ recquirementsPhase.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class Recquirements {
+    public:
+        void executeRecquirements() {
+            cout << "Iniciando fase de requisitos..." << endl;
+            cout << "Iniciando estudo de viabilidade..."  << endl;
+            cout << "Elicitação e análise dos requisitos..." << endl;
+            cout << "Especificando requisitos..." << endl;
+            cout << "Validando requisitos..." << endl;
+        }
+};
